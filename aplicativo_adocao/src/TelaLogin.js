f677d3a56f26dcdf91fc376a2f8a2d16796811ba
RodrigosRockenbach <rodrockenbach@gmail.com>
2022-10-11 23:42:03 -0300

    Iniciando a primeira etapa do projeto

---
+++ aplicativo_adocao/src/TelaLogin.js
@@ -0,0 +1,13 @@
+function TelaLogin() {
+    return (
+      <form className="formLogin">
+        <span>
+          <p>IMG</p>
+        </span>
+        <span>LOGIN</span>
+      </form>
+      
+    );
+  }
+  
+  export default TelaLogin;
